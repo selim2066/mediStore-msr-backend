6f5d1d9bec40332cb9045b3fe2a5732f948fb4a7
selim2066 <mdselimreza2066@gmail.com>
2026-04-11 15:31:41 +0600

    fix: update prisma client import paths

--- src/modules/orders/order.service.ts
+++ src/modules/orders/order.service.ts
@@ -1,4 +1,5 @@
-import { OrderStatus } from "../../../generated/prisma/client";
+
+import { OrderStatus } from "../../generated/prisma";
 import { prisma } from "../../lib/prisma";
 
 // todo #1 create order with items and deduct stock
