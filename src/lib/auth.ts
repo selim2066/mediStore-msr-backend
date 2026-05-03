e03fe8dd9cdd4d2cca123b4b5c0e8b17a01bab03
selim2066 <mdselimreza2066@gmail.com>
2026-05-03 15:14:27 +0600

    google login credential

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -218,6 +218,14 @@ This verification link may expire for security reasons.<br/>
     },
   },
 
+  socialProviders: {
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    
+  },
+},
+
   user: {
     additionalFields: {
       role: {
